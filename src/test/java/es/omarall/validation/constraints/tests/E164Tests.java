db6dfef6bae74544c402d55478eea0208f987766
oalles <oalles@neivi.es>
2016-02-15 11:23:45 +0100

    Some E164 tests

---
+++ src/test/java/es/omarall/validation/constraints/tests/E164Tests.java
@@ -0,0 +1,71 @@
+package es.omarall.validation.constraints.tests;
+
+import java.util.Set;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.Validator;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.AbstractJUnit4SpringContextTests;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.util.Assert;
+
+import es.omarall.validation.constraints.E164;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = { TestConfiguration.class })
+public class E164Tests extends AbstractJUnit4SpringContextTests {
+
+    // private Logger LOG = LoggerFactory.getLogger(E164Tests.class);
+
+    @Autowired
+    private Validator validator;
+
+    /**
+     * Class to test
+     */
+    public class PhoneNumberWrapper {
+
+        @E164
+        private final String phoneNumber;
+
+        public PhoneNumberWrapper(String phoneNumber) {
+            this.phoneNumber = phoneNumber;
+        }
+    }
+
+    @Before
+    public void beforeTest() throws Exception {
+    }
+
+    @Test
+    public void testE164IsValid() throws Exception {
+        PhoneNumberWrapper w = new PhoneNumberWrapper("+34600000000");
+        Set<ConstraintViolation<PhoneNumberWrapper>> constraintViolations = validator
+                .validate(w);
+        Assert.isTrue(0 == constraintViolations.size());
+    }
+
+    @Test
+    public void testNoPlusSignIsInvalid() throws Exception {
+
+        PhoneNumberWrapper w = new PhoneNumberWrapper("34600000000");
+        Set<ConstraintViolation<PhoneNumberWrapper>> constraintViolations = validator
+                .validate(w);
+        Assert.isTrue(1 == constraintViolations.size());
+    }
+
+    @Test
+    public void testNumberWithPlusSignIsInvalid() throws Exception {
+        PhoneNumberWrapper w = new PhoneNumberWrapper("+34 600 00 00 00");
+
+        Set<ConstraintViolation<PhoneNumberWrapper>> constraintViolations = validator
+                .validate(w);
+        Assert.isTrue(1 == constraintViolations.size());
+    }
+
+}
